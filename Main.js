ca8197792747f40c77bfe4e27b6348869a3bcc0e
JPbooya <perezjcarlos81@gmail.com>
2025-10-21 20:12:29 -0700

    added search bar Mechanisim logic

--- Main.js
+++ Main.js
@@ -10,4 +10,30 @@ for(var btn of addToListBtns){
     })
 };
 
+/* Search Bar Mechanisim */
+// Note: this is only currently working for the tournamentItems.html page
+document.addEventListener('DOMContentLoaded',()=> {
+    const searchInput = document.getElementById('q');
+    // event listener for users input
+    if (searchInput) {
+        searchInput.addEventListener('keyup', filterProducts);
+    }
+    // function to filter products based on user input
+    function filterProducts() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const products = document.querySelectorAll('.product-item');
+
+        // looping through each product for user input match if corresponds
+        // too h3 value
+        products.forEach(product => {
+            const productText = product.querySelector('h3').textContent.toLowerCase();
+
+            if (productText.includes(searchTerm)) {
+                product.style.display = 'block'; // shows the product
+            } else {
+                product.style.display = 'none'; // hides the product
+            }
+        });
+    }
+});
 
