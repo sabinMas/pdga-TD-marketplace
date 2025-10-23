98109561d24fbb669158efd3dd155924d440c92b
Cesar Dato <hellocesar3@gmail.com>
2025-10-22 17:26:25 -0700

    added 'addToList' class to all add to list buttons | also added a eventID search and moved the search park below

--- Main.js
+++ Main.js
@@ -3,12 +3,13 @@ const numCheckOutref = document.getElementById("numCheckOut");
 var numCheckOut = 0;
 
 for(var btn of addToListBtns){
-    btn.addEventListener('click',()=>{
+    btn.addEventListener('click', ()=> {
         console.log("added to list");
         numCheckOut++;
         numCheckOutref.innerHTML="Check Out: " + numCheckOut;
     })
 };
+
 
 /* Search Bar Mechanisim */
 // Note: this is only currently working for the tournamentItems.html page
