76c16f278a78b018db633f219619059edae5dd40
JPbooya <perezjcarlos81@gmail.com>
2025-10-25 21:49:43 -0700

    fixed search logic and placed search logic code combined with chart/checkout code

--- Main.js
+++ Main.js
@@ -70,34 +70,8 @@ listBtn.addEventListener('click', function() {
   }
 });
 
-
-/* Search Bar Mechanisim */
-// Note: this is only currently working for the tournamentItems.html page
-document.addEventListener('DOMContentLoaded', () => {
-  const searchInput = document.getElementById('q');
-  // event listener for users input
-  if (searchInput) {
-    searchInput.addEventListener('keyup', filterProducts);
-  }
-  // function to filter products based on user input
-  function filterProducts() {
-    const searchTerm = searchInput.value.toLowerCase();
-    const products = document.querySelectorAll('.product-item');
-
-    // looping through each product for user input match if corresponds
-    // too h3 value
-    products.forEach((product) => {
-      const productText = product.querySelector('h3').textContent.toLowerCase();
-
-      if (productText.includes(searchTerm)) {
-        product.style.display = 'block'; // shows the product
-      } else {
-        product.style.display = 'none'; // hides the product
-      }
-    });
-  }
-
   document.addEventListener('DOMContentLoaded', () => {
+    const searchInput = document.getElementById('q');
     const saved = JSON.parse(localStorage.getItem('pdga_user') || 'null');
     const userInfo = document.getElementById('userInfo');
     const guestInfo = document.getElementById('guestInfo');
@@ -114,10 +88,14 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       guestInfo.hidden = false;
     }
+
+    if (searchInput) {
+      searchInput.addEventListener('keyup', filterProducts)
+    }
     // function to filter products based on user input
     function filterProducts() {
         const searchTerm = searchInput.value.toLowerCase();
-        const products = document.querySelectorAll('.product-item');
+        const products = document.querySelectorAll('.product-card');
 
         // looping through each product for user input match if corresponds
         // too h3 value
@@ -131,6 +109,4 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
-}); 
-
 });
