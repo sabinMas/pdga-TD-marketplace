68a91db6e16508db82773b4c8ad5cda5e30f34fc
sabinMas <sabin.mason@student.greenriver.edu>
2025-11-23 14:57:23 -0800

    fixed database.php

--- phpFiles/database.php
+++ phpFiles/database.php
@@ -3,7 +3,7 @@
 
 $DB_HOST = 'localhost';
 $DB_NAME = 'interfa1_pdga_inventory';  // <-- your REAL DB name
-$DB_USER = 'interfa1_interfa1';          // <-- your REAL DB user
+$DB_USER = 'interfa1_pdga';          // <-- your REAL DB user
 $DB_PASS = 'Pxg!k45P73*OWc';  // <-- paste your actual password
 
 $dsn = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4";
