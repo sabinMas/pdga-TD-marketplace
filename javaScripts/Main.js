7333507bdee94afa6053848497706596baa35834
Anthony M <162858846+AnthonyMLDZ@users.noreply.github.com>
2025-12-03 15:19:16 -0800

    more changes to main, still work in progress

--- javaScripts/Main.js
+++ javaScripts/Main.js
@@ -311,3 +311,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+// fade in javascript
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
